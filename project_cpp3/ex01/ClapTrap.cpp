830bb568683e60d35e423541e3132d1a3510bc83
Catnox <dubos.raphael@gmail.com>
2025-12-07 19:56:32 +0100

    go

---
+++ project_cpp3/ex01/ClapTrap.cpp
@@ -0,0 +1,75 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap() : _name("Default"), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap " << _name << " constructed with default values." << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string &name) : _name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0)
+{
+	std::cout << "ClapTrap " << _name << " constructed." << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other)
+{
+	*this = other;
+	std::cout << "ClapTrap " << _name << " copy constructed." << std::endl;
+}
+
+ClapTrap &ClapTrap::operator=(const ClapTrap &other)
+{
+	if (this != &other)
+	{
+		_name = other._name;
+		_hitPoints = other._hitPoints;
+		_energyPoints = other._energyPoints;
+		_attackDamage = other._attackDamage;
+	}
+	return *this;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "ClapTrap " << _name << " destructed." << std::endl;
+}
+
+void ClapTrap::attack(const std::string &target)
+{
+	if (_energyPoints > 0 && _hitPoints > 0)
+	{
+		_energyPoints--;
+		std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << _name << " has no energy or hit points left to attack." << std::endl;
+	}
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_hitPoints <= amount)
+	{
+		_hitPoints = 0;
+		std::cout << "ClapTrap " << _name << " takes " << amount << " points of damage and is now destroyed!" << std::endl;
+	}
+	else
+	{
+		_hitPoints -= amount;
+		std::cout << "ClapTrap " << _name << " takes " << amount << " points of damage, remaining hit points: " << _hitPoints << "." << std::endl;
+	}
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (_energyPoints > 0 && _hitPoints > 0)
+	{
+		_energyPoints--;
+		_hitPoints += amount;
+		std::cout << "ClapTrap " << _name << " is repaired by " << amount << " points, total hit points: " << _hitPoints << "." << std::endl;
+	}
+	else
+	{
+		std::cout << "ClapTrap " << _name << " has no energy or hit points left to repair." << std::endl;
+	}
+}
